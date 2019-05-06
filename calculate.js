b9f55865e4c023940cd847314ac1736f1b5ae354
erwinnurhaq <simkalastoforka@gmail.com>
2019-05-06 22:33:49 +0700

    add z-index in flex

--- calculate.js
+++ calculate.js
@@ -1,28 +1,25 @@
-var a,
-    b,
-    c,
-    d,
-    hasilnettbundle,
-    hasilnettunit;
+var a, b, c, d, hasilnettbundle, hasilnettunit;
 
 function hitung() {
-    a = document.getElementById("harganormal").value;
-    b = document.getElementById("jumlahunit").value;
-    c = document.getElementById("diskon1").value;
-    d = document.getElementById("diskon2").value;
+  a = document.getElementById("harganormal").value;
+  b = document.getElementById("jumlahunit").value;
+  c = document.getElementById("diskon1").value;
+  d = document.getElementById("diskon2").value;
 
-    hasilnettbundle = a - (a * (c / 100));
-    hasilnettbundle -= (hasilnettbundle * (d / 100));
-    hasilnettunit = hasilnettbundle / b;
-    document.getElementById("hasilnettbundle").innerHTML = "Rp. " + hasilnettbundle.toFixed(2) + " /bundle";
-    document.getElementById("hasilnettunit").innerHTML = "Rp. " + hasilnettunit.toFixed(2) + " /unit";
+  hasilnettbundle = a - a * (c / 100);
+  hasilnettbundle -= hasilnettbundle * (d / 100);
+  hasilnettunit = hasilnettbundle / b;
+  document.getElementById("hasilnettbundle").innerHTML =
+    "Rp. " + hasilnettbundle.toFixed(2) + " /bundle";
+  document.getElementById("hasilnettunit").innerHTML =
+    "Rp. " + hasilnettunit.toFixed(2) + " /unit";
 }
 
 function kosongkan() {
-    document.getElementById("harganormal").value = "";
-    document.getElementById("jumlahunit").value = "";
-    document.getElementById("diskon1").value = "";
-    document.getElementById("diskon2").value = "";
-    document.getElementById("hasilnettbundle").innerHTML = "Rp. ";
-    document.getElementById("hasilnettunit").innerHTML = "Rp. ";
+  document.getElementById("harganormal").value = "";
+  document.getElementById("jumlahunit").value = "";
+  document.getElementById("diskon1").value = "";
+  document.getElementById("diskon2").value = "";
+  document.getElementById("hasilnettbundle").innerHTML = "Rp. -";
+  document.getElementById("hasilnettunit").innerHTML = "Rp. -";
 }
